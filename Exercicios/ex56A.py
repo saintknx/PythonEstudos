0c8553ebbdaea03c6a7a2498db757feeb15703c7
Kaua Sousa <kauasantos2014s10@gmail.com>
2025-10-24 15:42:54 -0300

    ajustes

--- Exercicios/ex56A.py
+++ Exercicios/ex56A.py
@@ -6,15 +6,19 @@ totmulher21 = 0
 hidade = 0
 for c in range(1, 5):
     print('-=-=-{}ª PESSOA-=-=-'.format(c))
-    nome = str(input('Nome: ')).strip()
-    idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()
+    nome = str(input('Nome: '.format(c))).strip()
+    idade = int(input('Idade: '.format(c)))
+    sexo = str(input('Sexo [M/F]: '.format(c))).strip().upper()
     totidade += idade
-    if sexo[0] == 'M' and idade > hidade:
+    if c == 1 and sexo[0] == 'M':
         hvelho = nome
         hidade = idade
-    if sexo[0] == 'F' and idade < 20:
-        totmulher21 += 1
-print('A média de idade entre as 4 pessoas é de {:.1f}'.format(totidade / 4))
+    if sexo[0] == 'M' and idade > hidade:
+        hvelho = nome 
+        hidade = idade
+    else:
+        if sexo[0] == 'F' and idade < 20:
+            totmulher21 += 1
+print('A média de idade entre as 4 pessoas é de {}'.format(totidade / 4))
 print('O homem mais velho é {} com {} anos'.format(hvelho.capitalize(), hidade))
 print('Ao todo, a quantidade de mulheres com menos de 20 anos é {}'.format(totmulher21))
