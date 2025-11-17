ec83c65a567e7045f6b63f14fd4d24a02f50bce2
Kaua Sousa <kauasantos2014s10@gmail.com>
2025-11-17 17:56:41 -0300

    lista final pt1

--- Exercicios/ex83.py
+++ Exercicios/ex83.py
@@ -1,2 +1,17 @@
 '''Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+expressao = str(input('Digite a expressão: '))
+pilha = []
+for simb in expressao:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está inválida!')
